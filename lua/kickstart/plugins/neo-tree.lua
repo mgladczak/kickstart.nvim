29e8337a61d058e2a5ca3bd56205acf2c33d2ae9
mgladczak <mateusz@gladczak.com>
2024-10-23 09:56:36 +0200

    Update neo-tree.lua

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -17,12 +17,8 @@ return {
     filesystem = {
       visible = true,
       show_hidden_count = true,
-	    hide_dotfiles = false,
-	    hide_gitignored = true,
-	    hide_by_name = {
-	       -- '.git',
-	       -- '.DS_Store',
-	       -- 'thumbs.db',
+      hide_dotfiles = false,
+      hide_gitignored = true,
       },
       window = {
         mappings = {
