f5e57516be2c64a5388296b75aadd46ae904b27b
mgladczak <mateusz@gladczak.com>
2024-10-24 19:08:12 +0200

    Update cheatsheet.lua

--- lua/custom/plugins/cheatsheet.lua
+++ lua/custom/plugins/cheatsheet.lua
@@ -1,19 +1,8 @@
--- nvim v0.8.0
-return {
-    "sudormrfbin/cheatsheet.nvim",
-    lazy = true,
-    cmd = {
-        "CheatSheet", 
-     },
-    -- optional for floating window border decoration
-    dependencies = {
-        "nvim-telescope/telescope.nvim",
-        "nvim-lua/popup.nvim",
-        "nvim-lua/plenary.nvim",
-    },
-    -- setting the keybinding for LazyGit with 'keys' is recommended in
-    -- order to load the plugin when the command is run for the first time
-    keys = {
-        { "<leader>cs", "<cmd>CheatSheet<cr>", desc = "CheatSheet" }
-    }
+use {
+  'sudormrfbin/cheatsheet.nvim',
+  requires = {
+    {'nvim-telescope/telescope.nvim'},
+    {'nvim-lua/popup.nvim'},
+    {'nvim-lua/plenary.nvim'},
+  }
 }
