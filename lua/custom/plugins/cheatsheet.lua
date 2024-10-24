9aa8f8527f503079b001c5feef749bf94c57e19c
mgladczak <mateusz@gladczak.com>
2024-10-24 19:11:42 +0200

    Update cheatsheet.lua

--- lua/custom/plugins/cheatsheet.lua
+++ lua/custom/plugins/cheatsheet.lua
@@ -1,8 +1,14 @@
-use {
-  'sudormrfbin/cheatsheet.nvim',
-  requires = {
-    {'nvim-telescope/telescope.nvim'},
-    {'nvim-lua/popup.nvim'},
-    {'nvim-lua/plenary.nvim'},
-  }
+-- nvim v0.8.0
+return {
+    "sudormrfbin/cheatsheet.nvim",
+    lazy = true,
+    cmd = {
+        "CheatSheet", 
+     },
+    -- optional for floating window border decoration
+    dependencies = {
+        "nvim-telescope/telescope.nvim",
+        "nvim-lua/popup.nvim",
+        "nvim-lua/plenary.nvim",
+    },
 }
