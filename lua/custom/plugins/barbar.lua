f0f831bc3546be71be84ea4abf1dda0234b7eff9
mgladczak <mateusz@gladczak.com>
2024-10-29 09:19:43 +0100

    Update barbar.lua

--- lua/custom/plugins/barbar.lua
+++ lua/custom/plugins/barbar.lua
@@ -8,6 +8,10 @@ return {
   init = function()
     vim.g.barbar_auto_setup = false
   end,
+  config = function()
+    vim.keymap.set('n', '<A-,>', '<Cmd>BufferPrevious<CR>', { noremap = true, silent = true })
+    vim.keymap.set('n', '<A-.>', '<Cmd>BufferNext<CR>', { noremap = true, silent = true })
+  end,
   opts = {
     animation = true,
   },
