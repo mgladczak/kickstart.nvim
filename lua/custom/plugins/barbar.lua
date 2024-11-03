d12d8dfd6586515aba7618104c3500767ccaae4c
mgladczak <mateusz@gladczak.com>
2024-11-03 13:16:15 +0100

    Update barbar.lua

--- lua/custom/plugins/barbar.lua
+++ lua/custom/plugins/barbar.lua
@@ -1,16 +1,16 @@
-return {
-  'romgrk/barbar.nvim',
-  version = '*',
-  dependencies = {
-    'lewis6991/gitsigns.nvim',
-    'nvim-tree/nvim-web-devicons',
-  },
-  init = function()
-    vim.g.barbar_auto_setup = false
-  end,
-  config = function()
-  end,
-  opts = {
-    animation = true,
-  },
-}
+-- return {
+--   'romgrk/barbar.nvim',
+--   version = '*',
+--   dependencies = {
+--     'lewis6991/gitsigns.nvim',
+--     'nvim-tree/nvim-web-devicons',
+--   },
+--   init = function()
+--     vim.g.barbar_auto_setup = false
+--   end,
+--   config = function()
+--   end,
+--   opts = {
+--     animation = true,
+--   },
+-- }
