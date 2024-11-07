9e933c87767b9bc931939b89e27b8d1adf4f1eee
mgladczak <mateusz@gladczak.com>
2024-11-07 15:39:08 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -847,6 +847,22 @@ require('lazy').setup({
 
   { -- Collection of various small independent plugins/modules
     'echasnovski/mini.nvim',
+    keys = {
+    {
+      "<leader>fm",
+      function()
+        require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
+      end,
+      desc = "Open mini.files (Directory of Current File)",
+    },
+    {
+      "<leader>fM",
+      function()
+        require("mini.files").open(vim.uv.cwd(), true)
+      end,
+      desc = "Open mini.files (cwd)",
+    },
+    },
     config = function()
       -- Better Around/Inside textobjects
       --
